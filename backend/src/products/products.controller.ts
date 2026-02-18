58680cf91bbeeda049a5ac9cc66f2df6f69fcc1e
Manattee Vilairat <6810110275@psu.ac.th>
2026-02-18 13:13:38 +0700

    test product controller

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller , Get, Param, NotFoundException} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return "Hello from products page";
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return "product";
+  }
 }
+
