e8027ba28f680425a945f97863cb34ec389042a1
Sorn <sorn@MacBookAirSorn-4.local>
2026-02-18 12:22:25 +0700

    create products module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
     imports: [
@@ -28,6 +29,7 @@ import { AuthModule } from './auth/auth.module';
         }),
         AuthModule,
         UsersModule,
+        ProductsModule,
     ],
     controllers: [AppController],
     providers: [AppService],
